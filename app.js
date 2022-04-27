5bade49a35ada51f0709e0cf568f4107f43016b9
Fábio Silva <fabio.miguel.marques.da.silva@gmail.com>
2022-04-27 22:32:44 +0200

    added further js logic, added html content

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// Available options 
 let computerOptions = ["Rock", "Paper", "Scissors"];
-let playerOptions = ["Rock", "Paper", "Scissors"];
+let playerOptions = ["Rock", "Paper", "Scissors"]; 
 
-let randomSelection = Math.floor(Math.random()*3);
+//random selection logic using math random number with the array of options length
+let randomSelection = Math.floor(Math.random()*computerOptions.length);
+let computerSelection = computerOptions[randomSelection];
 
+//console log to check random number if it's matching the computerSelection
 console.log(randomSelection);
+console.log(computerSelection);
